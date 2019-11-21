f643d505aa72663b671e2e088ee855ae094e06fb
Jerome Prinet <jerome.prinet@orange.com>
2019-11-22 00:06:25 +0100

    InsertionSort with treemap

--- test/src/main/java/InsertionSort.java
+++ test/src/main/java/InsertionSort.java
@@ -1,16 +1,24 @@
+import java.util.TreeMap;
+
 public class InsertionSort {
 
     static int insertionSort(int[] arr) {
         int nbShifts = 0;
+
+        TreeMap<Integer, Integer> ordered = new TreeMap<>();
+        ordered.put(arr[0], 1);
+
         for (int i = 1; i < arr.length; i++) {
-            int j = i;
-            while (j > 0 && arr[j - 1] > arr[j]) {
-                nbShifts++;
-                int tmp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = tmp;
-                j--;
+            if (ordered.lastKey() > arr[i]) {
+                for (Integer current : ordered.descendingKeySet()) {
+                    if (current > arr[i]) {
+                        nbShifts += ordered.get(current);
+                    } else {
+                        break;
+                    }
+                }
             }
+            ordered.put(arr[i], ordered.get(arr[i]) != null ? ordered.get(arr[i]) + 1 : 1);
         }
         return nbShifts;
     }
