0374e86a8434e70c7190f86a1d20b4475391a5b5
Jerome Prinet <jerome.prinet@orange.com>
2019-11-24 20:45:27 +0100

    Leaderboard

--- src/main/java/LeaderBoard.java
+++ src/main/java/LeaderBoard.java
@@ -1,24 +1,25 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 class LeaderBoard {
 
     static int[] climbingLeaderboard(int[] scores, int[] alice) {
         int[] leaderboardProgression = new int[alice.length];
 
-        java.util.List<Integer> orderedScores = java.util.Arrays.stream(scores)
-                                                                .boxed()
-                                                                .distinct()
-                                                                .sorted(java.util.Comparator.reverseOrder())
-                                                                .collect(java.util.stream.Collectors.toList());
+        Object[] orderedScores = Arrays.stream(scores)
+                                       .boxed()
+                                       .distinct()
+                                       .sorted(Comparator.reverseOrder())
+                                       .toArray();
 
         for (int i = 0; i < alice.length; i++) {
-            int currentRank = 1;
-            for (Integer highScore : orderedScores) {
-                if (alice[i] >= highScore) {
-                    break;
-                } else {
-                    currentRank++;
-                }
+            int index = Arrays.binarySearch(orderedScores, alice[i], Collections.reverseOrder());
+            if (index < 0) {
+                leaderboardProgression[i] = Math.abs(index);
+            } else {
+                leaderboardProgression[i] = index + 1;
             }
-            leaderboardProgression[i] = currentRank;
         }
 
         return leaderboardProgression;
